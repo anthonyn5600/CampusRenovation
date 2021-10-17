4040ef552bc2d4b09b84ed596883cd608d17a378
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-10-17 10:22:56 -0700

    Appearance and System
    Change some ui elements appearance and implemented a routing.

---
+++ routing.js
@@ -0,0 +1,10 @@
+let accessToken = 'REDACTED_MAPBOX_TOKEN'
+
+// L.Routing.control({
+//     waypoints: [
+//         L.latLng(33.88504928033317,-117.88942694664001),
+//         L.latLng(33.88032863749295,-117.88177728652956),
+//     ],
+//     router: L.Routing.mapbox(accessToken, {profile: 'mapbox/walking'})
+
+// }).addTo(map);
