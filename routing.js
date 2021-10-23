554e51ca1ab1a67d120d5d3756e36ab1fc82652c
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-10-22 21:17:12 -0700

    Remove comments

--- routing.js
+++ routing.js
@@ -2,8 +2,8 @@ let accessToken = 'sk.eyJ1IjoiZGFya2xpZ2h0MXgiLCJhIjoiY2t1dXd0NW40MWlnaTJ3bzZ4cj
 
 // L.Routing.control({
 //     waypoints: [
-//         L.latLng(33.88504928033317,-117.88942694664001),
-//         L.latLng(33.88032863749295,-117.88177728652956),
+//         L.latLng(33.88039098730793,-117.88849353790285),
+//         L.latLng(33.88231100528884,-117.88264095783235),
 //     ],
 //     router: L.Routing.mapbox(accessToken, {profile: 'mapbox/walking'})
 
