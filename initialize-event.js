42169ad5f13b124a10175777e394b764dd86934c
Anthony Ngon <37987501+anthonyn5600@users.noreply.github.com>
2021-12-03 00:00:40 -0800

    Created comments

--- initialize-event.js
+++ initialize-event.js
@@ -1,10 +1,12 @@
 var event_location = []
+//Serve the purpose of using a GET Request toward the Github Gist website and store it into an variable
 async function fetchEvent() {
     const response = await fetch('https://gist.githubusercontent.com/anthonyn5600/42f598c9b99ba68207cce175605b5578/raw/99d1c41e6a6f52a5ec81bfc745a3b95e0e33ecf0/event.json')
         .then(response => response.json())
         .then(data => showEvents(data.events));
 }
 
+//Initialize the events based off the data that store the information from the GET Request
 function showEvents(data) {
     const eventcontainer = document.getElementById('event-scroll')
     for (var i = 0; i < data.length; i++) {
@@ -17,7 +19,7 @@ function showEvents(data) {
                             Time: ${data[i].event_time}<br/>
                             <a href="${data[i].link}" target="popup" onclick="window.open(${data[i].link},'popup','width=600,height=600'); return false;">More Info</a><br/>
                             <button onclick="routeTo('${data[i].location}')">Get Direction</button>`
-
+            
         if (!event_location.includes(data[i].location)) {
             event_location.push(data[i].location)
         }
